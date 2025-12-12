2b052685a36cf70da3acabfc6ced809ed2a26bab
chaitanya461 <saipureddysai17@gmail.com>
2025-12-12 12:31:23 +0530

    Update main.tf

--- modulesproject/modules/vpc/main.tf
+++ modulesproject/modules/vpc/main.tf
@@ -5,3 +5,14 @@ resource "aws_vpc" "this" {
     Name = var.vpc_name
   }
 }
+
+resource "aws_subnet" "public_a" {
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = "192.168.1.0/24" # Adjust CIDR as needed
+  availability_zone = "ap-south-1a" # Adjust AZ to match your region
+
+  tags = {
+    Name = "${var.vpc_name}-public-a"
+  }
+}
+~
